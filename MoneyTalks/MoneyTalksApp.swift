faaa421b6cc1590bc1730e832d4b570127066249
refractored <fatboy11905@gmail.com>
2023-05-09 10:23:59 -0500

    Initial Commit

---
+++ MoneyTalks/MoneyTalksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTalksApp.swift
+//  MoneyTalks
+//
+//  Created by David M. Galvan on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTalksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
