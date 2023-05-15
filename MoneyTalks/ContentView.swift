2e0a882af30de306a55df9d9afabab1717c20718
refractored <fatboy11905@gmail.com>
2023-05-15 10:52:29 -0500

    Init home

--- MoneyTalks/ContentView.swift
+++ MoneyTalks/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let gradient = LinearGradient(colors: [Color.black,Color.green],
+                                  startPoint: .top, endPoint: .bottom)
     var body: some View {
-        VStack {
-            //TEST
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, test!")
+        NavigationStack {
+            ZStack {
+                gradient
+                    .opacity(0.25)
+                    .ignoresSafeArea()
+                
+                VStack {
+
+                    Text("$20")
+                        .font(.system(size: 80))
+                        .frame(width: 5000)
+                        .foregroundColor(.green)
+                }
+                .navigationTitle("Home")
+                .font(.title2)
+            }
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
-}
+} 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
